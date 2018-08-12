bf27ba0317f1c5fe2b39b4f194dd63448769ab9f
Rajvir Rai <rsr1992@gmail.com>
2018-08-12 19:04:07 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
+
+# def square_array(numbers)
+#   numbers = [1,2,3]
+#   numbers.each { |num| puts "#{num * num}"}
+#   new_array = numbers.map puts "#{num}"
+# end
+
+
 def square_array(array)
-  # your code here
+  return_array = []
+  
+  array.each do |num|
+    return_array.push num*num
+    end
+    return return_array
 end
